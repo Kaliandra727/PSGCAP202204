f10aead5128120b256c603e74c8c886ebb08b44d
KALIANDRA DE FREITAS PEREIRA <100894917+Kaliandra727@users.noreply.github.com>
2022-04-08 14:23:19 -0400

    Add files via upload

---
+++ encontro5_insertinto.sql
@@ -0,0 +1,14 @@
+--SELECT * FROM Produto
+--GO
+
+INSERT INTO Produto( ID_Produto,Descricao_Produto,Preco_Produto)
+VALUES 
+   (3, 'Pepsi lata 150ml', 1.50),
+   (4, 'Guarána Jesus 150ml', 1.50),
+   (5, 'Ice Cola 150ml', 1.50),
+   (6, 'Pepsi Twist 150ml', 1.50),
+   (7, 'Guaraná Antártica lata 150ml', 1.50),
+   (8, 'Dolly 150ml', 1.50),
+   (9, 'Sprite 150ml', 1.50),
+   (10,'H2o 150ml', 1.50)
+   go
